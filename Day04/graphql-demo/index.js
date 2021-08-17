9025815733468f9e0356e9fe7b2b25004e0b17d3
家暐 莊 <amos6572@hotmail.com>
2021-08-17 14:03:32 +0800

    Day06: Schema 與 Resolver 進階功能！ (Array, Non-Null, Field Resolver)

--- Day04/graphql-demo/index.js
+++ Day04/graphql-demo/index.js
@@ -2,31 +2,19 @@ const { ApolloServer, gql } = require('apollo-server');
 // ApolloServer: 讓我們啟動 server 的 class ，不但實作許多 GraphQL 功能也提供 web application 的功能 (背後使用 express)
 // gql: template literal tag, 讓你在 Javascript 中使用 GraphQL 語法
 
-// 1. 加入假資料
+// 1. 在假資料中補充朋友資訊
 const users = [
-    {
-      id: 1,
-      name: 'Fong',
-      age: 23
-    },
-    {
-      id: 2,
-      name: 'Kevin',
-      age: 40
-    },
-    {
-      id: 3,
-      name: 'Mary',
-      age: 18
-    }
+    { id: 1, name: 'Fong', age: 23, friendIds: [2, 3] },
+    { id: 2, name: 'Kevin', age: 40, friendIds: [1] },
+    { id: 3, name: 'Mary', age: 18, friendIds: [1] }
   ];
 
 
 // The GraphQL schema
-// 2. 新增 User type 、在 Query 中新增 me field
+// 2. 在 Schema 添加新 fields
 const typeDefs = gql`
   """
-  使用者資訊
+  使用者
   """
   type User {
     "識別碼"
@@ -35,6 +23,8 @@ const typeDefs = gql`
     name: String
     "年齡"
     age: Int
+    "朋友們"
+    friends: [User]
   }
 
   type Query {
@@ -42,18 +32,31 @@ const typeDefs = gql`
     hello: String
     "取得當下使用者"
     me: User
+    "取得所有使用者"
+    users: [User]
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
     Query: {
-      // 需注意名稱一定要對到 Schema 中 field 的名稱  
       hello: () => 'world',
-      // 3. 加上 me 的 resolver (一定要在 Query 中喔)
-      me: () => users[0]
+      me: () => users[0],
+      // 3-1 在 `Query` 裡新增 `users`
+      users: () => users
+    },
+    // 3-2 新增 `User` 並包含 `friends` 的 field resolver
+    User: {
+      // 每個 Field Resolver 都會預設傳入三個參數，
+      // 分別為上一層的資料 (即 user)、參數 (下一節會提到) 以及 context (全域變數)
+      friends: (parent, args, context) => {
+        // 從 user 資料裡提出 friendIds
+        const { friendIds } = parent;
+        // Filter 出所有 id 出現在 friendIds 的 user
+        return users.filter(user => friendIds.includes(user.id));
+      }
     }
-};
+  };
 
 
 // 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
