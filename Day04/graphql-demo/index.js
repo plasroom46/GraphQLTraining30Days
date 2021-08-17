73a5694a65e2b9d9711efb594810845462319283
家暐 莊 <amos6572@hotmail.com>
2021-08-17 15:55:25 +0800

    Day07: Arguments, Aliases, Fragment 讓 Query 更好用 (進階 Query)

--- Day04/graphql-demo/index.js
+++ Day04/graphql-demo/index.js
@@ -4,15 +4,41 @@ const { ApolloServer, gql } = require('apollo-server');
 
 // 1. 在假資料中補充朋友資訊
 const users = [
-    { id: 1, name: 'Fong', age: 23, friendIds: [2, 3] },
-    { id: 2, name: 'Kevin', age: 40, friendIds: [1] },
-    { id: 3, name: 'Mary', age: 18, friendIds: [1] }
+    { id: 1, name: 'Fong', age: 23, height:170.0,weight:80.6, friendIds: [2, 3] },
+    { id: 2, name: 'Kevin', age: 40,height:185.3,weight:75.4, friendIds: [1] },
+    { id: 3, name: 'Mary', age: 18,height:165.2,weight:64.7, friendIds: [1] }
   ];
 
 
 // The GraphQL schema
 // 2. 在 Schema 添加新 fields
 const typeDefs = gql`
+  # Enum Type 為一種特殊的 Scalar Type ，使用時只能出現裡面有定義到的值且不需要加引號
+  # 進入 JavaSript 中使用時，會轉為 String 格式
+  """
+  高度單位
+  """
+  enum HeightUnit {
+    "公尺"
+    METRE
+    "公分"
+    CENTIMETRE
+    "英尺 (1 英尺 = 30.48 公分)"
+    FOOT
+  }
+  
+  """
+  重量單位
+  """
+  enum WeightUnit {
+    "公斤"
+    KILOGRAM
+    "公克"
+    GRAM
+    "磅 (1 磅 = 0.45359237 公斤)"
+    POUND
+  }
+
   """
   使用者
   """
@@ -23,6 +49,10 @@ const typeDefs = gql`
     name: String
     "年齡"
     age: Int
+    "身高 (預設為 CENTIMETRE)"
+    height(unit: HeightUnit = CENTIMETRE): Float
+    "體重 (預設為 KILOGRAM)"
+    weight(unit: WeightUnit = KILOGRAM): Float
     "朋友們"
     friends: [User]
   }
@@ -34,6 +64,8 @@ const typeDefs = gql`
     me: User
     "取得所有使用者"
     users: [User]
+    "取得特定 user (name 為必填)"
+    user(name: String!): User
   }
 `;
 
@@ -43,7 +75,13 @@ const resolvers = {
       hello: () => 'world',
       me: () => users[0],
       // 3-1 在 `Query` 裡新增 `users`
-      users: () => users
+      users: () => users,
+      // 對應到 Schema 的 Query.user
+      user: (root, args, context) => {
+        // 取出參數。因為 name 為 non-null 故一定會有值。
+        const { name } = args;
+        return users.find(user => user.name === name);
+      }
     },
     // 3-2 新增 `User` 並包含 `friends` 的 field resolver
     User: {
@@ -54,6 +92,25 @@ const resolvers = {
         const { friendIds } = parent;
         // Filter 出所有 id 出現在 friendIds 的 user
         return users.filter(user => friendIds.includes(user.id));
+      },
+      // 對應到 Schema 的 User.height
+      height: (parent, args) => {
+        const { unit } = args;
+        // 可注意到 Enum type 進到 javascript 就變成了 String 格式
+        // 另外支援 default 值 CENTIMETRE
+        if (!unit || unit === "CENTIMETRE") return parent.height;
+        else if (unit === "METRE") return parent.height / 100;
+        else if (unit === "FOOT") return parent.height / 30.48;
+        throw new Error(`Height unit "${unit}" not supported.`);
+      },
+      // 對應到 Schema 的 User.weight
+      weight: (parent, args, context) => {
+        const { unit } = args;
+        // 支援 default 值 KILOGRAM
+        if (!unit || unit === "KILOGRAM") return parent.weight;
+        else if (unit === "GRAM") return parent.weight * 100;
+        else if (unit === "POUND") return parent.weight / 0.45359237;
+        throw new Error(`Weight unit "${unit}" not supported.`);
       }
     }
   };
