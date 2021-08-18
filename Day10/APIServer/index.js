08a9ad93b40548139aa3034b9d84835b82385b7c
家暐 莊 <amos6572@hotmail.com>
2021-08-18 11:36:45 +0800

    Day10: 打造 GraphQL API Server 應用：部落格社交軟體 - 1 (Query & Mutation Part)

--- Day10/APIServer/index.js
+++ Day10/APIServer/index.js
@@ -2,23 +2,228 @@ const { ApolloServer, gql } = require('apollo-server');
 // ApolloServer: 讓我們啟動 server 的 class ，不但實作許多 GraphQL 功能也提供 web application 的功能 (背後使用 express)
 // gql: template literal tag, 讓你在 Javascript 中使用 GraphQL 語法
 
-// 1. GraphQL Schema 定義
+// 2. Mock Data & Field Resolver
+const meId = 1;
+const users = [
+  {
+    id: 1,
+    email: 'fong@test.com',
+    password: '$2b$04$wcwaquqi5ea1Ho0aKwkZ0e51/RUkg6SGxaumo8fxzILDmcrv4OBIO', // 123456
+    name: 'Fong',
+    age: 23,
+    friendIds: [2, 3]
+  },
+  {
+    id: 2,
+    email: 'kevin@test.com',
+    password: '$2b$04$uy73IdY9HVZrIENuLwZ3k./0azDvlChLyY1ht/73N4YfEZntgChbe', // 123456
+    name: 'Kevin',
+    age: 40,
+    friendIds: [1]
+  },
+  {
+    id: 3,
+    email: 'mary@test.com',
+    password: '$2b$04$UmERaT7uP4hRqmlheiRHbOwGEhskNw05GHYucU73JRf8LgWaqWpTy', // 123456
+    name: 'Mary',
+    age: 18,
+    friendIds: [1]
+  }
+];
+
+const posts = [
+  {
+    id: 1,
+    authorId: 1,
+    title: 'Hello World',
+    body: 'This is my first post',
+    likeGiverIds: [1, 2],
+    createdAt: '2018-10-22T01:40:14.941Z'
+  },
+  {
+    id: 2,
+    authorId: 2,
+    title: 'Nice Day',
+    body: 'Hello My Friend!',
+    likeGiverIds: [1],
+    createdAt: '2018-10-24T01:40:14.941Z'
+  }
+];
+
+
+// GraphQL Schema 定義
 const typeDefs = gql`
+  """
+  使用者
+  """
+  type User {
+    "識別碼"
+    id: ID!
+    "帳號 email"
+    email: String!
+    "名字"
+    name: String
+    "年齡"
+    age: Int
+    "朋友"
+    friends: [User]
+    "貼文"
+    posts: [Post]
+  }
+
+  """
+  貼文
+  """
+  type Post {
+    "識別碼"
+    id: ID!
+    "作者"
+    author: User
+    "標題"
+    title: String
+    "內容"
+    body: String
+    "按讚者"
+    likeGivers: [User]
+    "建立時間 (ISO 格式)"
+    createdAt: String
+  }
+  # 3-1. Query Type Schema
   type Query {
-    "A simple type for getting started!"
+    "測試用 Hello World"
     hello: String
+    "取得目前使用者"
+    me: User
+    "取得所有使用者"
+    users: [User]
+    "依照名字取得特定使用者"
+    user(name: String!): User
+    "取得所有貼文"
+    posts: [Post]
+    "依照 id 取得特定貼文"
+    post(id: ID!): Post
+  }
+
+  # 4-1. Mutation Type Schema
+  input UpdateMyInfoInput {
+    name: String
+    age: Int
+  }
+  
+  input AddPostInput {
+    title: String!
+    body: String
+  }
+  
+  type Mutation {
+    updateMyInfo(input: UpdateMyInfoInput!): User
+    addFriend(userId: ID!): User
+    addPost(input: AddPostInput!): Post
+    likePost(postId: ID!): Post
   }
 `;
 
-// 2. Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
+
+// helper functions
+// 2
+const filterPostsByUserId = userId =>
+  posts.filter(post => userId === post.authorId);
+
+const filterUsersByUserIds = userIds =>
+  users.filter(user => userIds.includes(user.id));
+  
+const findUserByUserId = userId => users.find(user => user.id === Number(userId));
+
+// 3-2. Query Type Resolver
+const findUserByName = name => users.find(user => user.name === name);
+const findPostByPostId = postId => posts.find(post => post.id === Number(postId));
+
+// 4-2. Mutation Type Resolver
+const updateUserInfo = (userId, data) =>
+  Object.assign(findUserByUserId(userId), data);
+
+const addPost = ({ authorId, title, body }) =>
+  (posts[posts.length] = {
+    id: posts[posts.length - 1].id + 1,
+    authorId,
+    title,
+    body,
+    likeGiverIds: [],
+    createdAt: new Date().toISOString()
+  });
+
+const updatePost = (postId, data) =>
+  Object.assign(findPostByPostId(postId), data);
+
+
+// Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
 const resolvers = {
+  // 3-2. Query Type Resolver
   Query: {
-    // 需注意名稱一定要對到 Schema 中 field 的名稱
-    hello: () => 'world'
+    hello: () => "world",
+    me: () => findUserByUserId(meId),
+    users: () => users,
+    user: (root, { name }, context) => findUserByName(name),
+    posts: () => posts,
+    post: (root, { id }, context) => findPostByPostId(id)
+  },
+  // 2.
+  User: {
+    posts: (parent, args, context) => filterPostsByUserId(parent.id),
+    friends: (parent, args, context) => filterUsersByUserIds(parent.friendIds || [])
+  },
+  Post: {
+    author: (parent, args, context) => findUserByUserId(parent.authorId),
+    likeGivers: (parent, args, context) =>
+      filterUsersByUserIds(parent.likeGiverIds)
+  },
+  // 4-2. Mutation Type Resolver
+  Mutation: {
+    updateMyInfo: (parent, { input }, context) => {
+      // 過濾空值
+      const data = ["name", "age"].reduce(
+        (obj, key) => (input[key] ? { ...obj, [key]: input[key] } : obj),
+        {}
+      );
+
+      return updateUserInfo(meId, data);
+    },
+    addFriend: (parent, { userId }, context) => {
+      const me = findUserByUserId(meId);
+      if (me.friendIds.include(userId))
+        throw new Error(`User ${userId} Already Friend.`);
+
+      const friend = findUserByUserId(userId);
+      const newMe = updateUserInfo(meId, {
+        friendIds: me.friendIds.concat(userId)
+      });
+      updateUserInfo(userId, { friendIds: friend.friendIds.concat(meId) });
+
+      return newMe;
+    },
+    addPost: (parent, { input }, context) => {
+      const { title, body } = input;
+      return addPost({ authorId: meId, title, body });
+    },
+    likePost: (parent, { postId }, context) => {
+      const post = findPostByPostId(postId);
+
+      if (!post) throw new Error(`Post ${postId} Not Exists`);
+
+      if (!post.likeGiverIds.includes(postId)) {
+        return updatePost(postId, {
+          likeGiverIds: post.likeGiverIds.concat(meId)
+        });
+      }
+
+      return updatePost(postId, {
+        likeGiverIds: post.likeGiverIds.filter(id => id === meId)
+      });
+    }
   }
 };
 
-// 3. 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
+// 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
 const server = new ApolloServer({
   // Schema 部分
   typeDefs,
@@ -26,7 +231,7 @@ const server = new ApolloServer({
   resolvers
 });
 
-// 4. 啟動 Server
+// 啟動 Server
 server.listen().then(({ url }) => {
   console.log(`? Server ready at ${url}`);
 });
