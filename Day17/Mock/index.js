3cf367a3171011d52b79a43b3840bf65faec9762
家暐 莊 <amos6572@hotmail.com>
2021-08-20 10:58:41 +0800

    Day17: GraphQL 入門： Apollo Mock - 做假資料好測試～ - MockList (帶參數一直傳不到後台，會報錯)

--- Day17/Mock/index.js
+++ Day17/Mock/index.js
@@ -1,11 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+
 const typeDefs = gql`
+
+type Person {
+  name: String
+  age: Int
+}
+
 type Query {
   hello: String
   getString: String
   getInt: Int
   getFloat: Float
+  people(first: Int): [Person]
 }
 `;
 
@@ -15,10 +23,20 @@ const resolvers = {
   },
 };
 
+var casual = require('casual');
+
 const mocks = {
   Int: () => 6,
   Float: () => 22.1,
   String: () => 'Hello',
+  Query: () =>({
+    people: (root, { first }, context) => {
+      if (first) {
+          return [...new Array(casual.integer(0, Math.round(Math.random()* (first+1) )))]
+      }
+        return [...new Array(casual.integer(0, Math.round(Math.random()*13)))]
+      }
+  }),
 };
 
 const server = new ApolloServer({
